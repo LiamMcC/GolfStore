2a160b403022d0f9e12335e5039c0b0dc7a61ee2
LmcCabe <me@liammccabe.ie>
2019-01-14 23:53:28 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+# If you leave this here when the user logges out it will break the application
+
+  
+end
